8d603cad6f46e1b1aef98008e330f473feb7e53a
Gyuchan <sb3g@naver.com>
2024-08-13 21:48:07 +0900

    ✨ feat: finish Chapter 5 on ORM

--- src/test/java/me/shinsunyoung/springbootdeveloper/MemberRepositoryTest.java
+++ src/test/java/me/shinsunyoung/springbootdeveloper/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.shinsunyoung.springbootdeveloper;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,58 +16,97 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+//    @Sql("/insert-members.sql")
+//    @Test
+//    void getAllMembers() {
+//        // when
+//        List<Member> members = memberRepository.findAll();
+//
+//        // then
+//        assertThat(members.size()).isEqualTo(3);
+//    }
+//
+//    @Sql("/insert-members.sql")
+//    @Test
+//    void getMemberById() {
+//        // when
+//        Member member = memberRepository.findById(2L).get();
+//
+//        // then
+//        assertThat(member.getName()).isEqualTo("B");
+//    }
+//
+//    @Sql("/insert-members.sql")
+//    @Test
+//    void getMemberByName() {
+//        // when
+//        Member member = memberRepository.findByName("C").get();
+//
+//        // then
+//        assertThat(member.getId()).isEqualTo(3);
+//    }
+//
+//    @Test       // 이미 추가된 데이터가 있으면 안 되므로, @Sql 어노테이션 사용 X
+//    void saveMember() {
+//        // given
+//        Member member = new Member(1L, "A");
+//
+//        // when
+//        memberRepository.save(member);
+//
+//        // then
+//        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+//    }
+//
+//    @Test
+//    void saveMembers() {
+//        // given
+//        List<Member> members = List.of(new Member(2L, "B"),
+//                new Member(3L, "C"));
+//
+//        // when
+//        memberRepository.saveAll(members);
+//
+//        // then
+//        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+//    }
+//
+//    @Sql("/insert-members.sql")
+//    @Test
+//    void deleteMemberById() {
+//        // when
+//        memberRepository.deleteById(2L);
+//
+//        // then
+//        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+//    }
+//
+//    @Sql("/insert-members.sql")
+//    @Test
+//    void deleteAll() {
+//        // when
+//        memberRepository.deleteAll();
+//
+//        // then
+//        assertThat(memberRepository.findAll().size()).isZero();
+//    }
+//
+//    @AfterEach
+//    public void cleanUp() {
+//        memberRepository.deleteAll();
+//    }
+
     @Sql("/insert-members.sql")
     @Test
-    void getAllMembers() {
-        // when
-        List<Member> members = memberRepository.findAll();
-
-        // then
-        assertThat(members.size()).isEqualTo(3);
-    }
-
-    @Sql("/insert-members.sql")
-    @Test
-    void getMemberById() {
-        // when
+    void update() {
+        // given
         Member member = memberRepository.findById(2L).get();
 
-        // then
-        assertThat(member.getName()).isEqualTo("B");
-    }
-
-    @Sql("/insert-members.sql")
-    @Test
-    void getMemberByName() {
         // when
-        Member member = memberRepository.findByName("C").get();
-        
-        // then
-        assertThat(member.getId()).isEqualTo(3);
-    }
-
-    @Test       // 이미 추가된 데이터가 있으면 안 되므로, @Sql 어노테이션 사용 X
-    void saveMember() {
-        // given
-        Member member = new Member(1L, "A");
-
-        // when
-        memberRepository.save(member);
+        member.changeName("BC");
 
         // then
-        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
     }
 
-    @Test
-    void saveMembers() {
-        // given
-        List<Member> members = List.of(new Member(2L, "B"),
-                new Member(3L, "C"));
-
-        // when
-        memberRepository.saveAll(members);
-
-        // then
-        assertThat(memberRepository.findAll().size()).isEqualTo(2);
-    }
 }
